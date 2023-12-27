40f79eceea0cbdc94d46a14f1f553187a352a761
Igor Paes <igor.paes@nextip.com.br>
2023-12-26 21:23:42 -0300

    feat: models

---
+++ lib/database/index.ts
@@ -0,0 +1,22 @@
+import mongoose, { mongo } from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+let cached = (global as any).mongoose || { coon: null, promise: null };
+
+export const connectToDatabase = async () => {
+  if (cached.conn) return cached.conn;
+
+  if (!MONGODB_URI) throw new Error("MONGODB_URI is not defined");
+
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URI, {
+      dbName: "evently",
+      bufferCommands: false,
+    });
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
+};
